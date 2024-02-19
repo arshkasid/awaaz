d480b6c162b0ee060745e5ff926227fa19253bcf
Arsh Kasid <arshkasid046@gmail.com>
2024-02-20 02:11:03 +0530

    Add files via upload

---
+++ awaaz.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: Feb 19, 2024 at 09:40 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `awaaz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accepted`
+--
+-- Error reading structure for table awaaz.accepted: #1932 - Table &#039;awaaz.accepted&#039; doesn&#039;t exist in engine
+-- Error reading data for table awaaz.accepted: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `awaaz`.`accepted`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allocation`
+--
+-- Error reading structure for table awaaz.allocation: #1932 - Table &#039;awaaz.allocation&#039; doesn&#039;t exist in engine
+-- Error reading data for table awaaz.allocation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `awaaz`.`allocation`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `houses`
+--
+-- Error reading structure for table awaaz.houses: #1932 - Table &#039;awaaz.houses&#039; doesn&#039;t exist in engine
+-- Error reading data for table awaaz.houses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `awaaz`.`houses`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+-- Error reading structure for table awaaz.requests: #1932 - Table &#039;awaaz.requests&#039; doesn&#039;t exist in engine
+-- Error reading data for table awaaz.requests: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `awaaz`.`requests`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `selected`
+--
+-- Error reading structure for table awaaz.selected: #1932 - Table &#039;awaaz.selected&#039; doesn&#039;t exist in engine
+-- Error reading data for table awaaz.selected: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `awaaz`.`selected`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table awaaz.users: #1932 - Table &#039;awaaz.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table awaaz.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `awaaz`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
